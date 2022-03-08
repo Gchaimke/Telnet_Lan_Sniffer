acd8ae1bb8cd465e459afb4bf14de52bb218e30a
Chaim <gchaimke@gmail.com>
2022-03-08 10:11:12 +0200

    working test

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 import re
 
-data = open("test.conf","r")
+data_file = open("test.conf", encoding='utf8')
+data = data_file.read()
+data_file.close()
 
-regex  = re.compile("(.+)->.+",re.MULTILINE)
 
-host_name = re.findall(regex, data.read())
-if(len(host_name)>0):
-    host_name=host_name[0]
+def get_hostname(string):
+    regex = re.compile("(.+)->.+", re.MULTILINE)
+    host_name = re.findall(regex, string)
+    if(len(host_name) > 0):
+        return host_name[0]
+    return "hostname_not_found"
 
-with open(f"out/{host_name}.conf","w") as  file:
-    for line in data.readlines():
-        if line == "\n":
+
+def remove_empty_lines(string):
+    new_string = ""
+    data = string.splitlines()
+    for line in data:
+        if line == "":
             continue
-        file.write(line)
+        new_string += line+"\n"
+    return new_string
 
+host = get_hostname(data)
+data = remove_empty_lines(data)
+
+txt = f"""
+new file name {host}
+{data}
+"""
+
+print(txt)
